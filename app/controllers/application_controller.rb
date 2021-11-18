c614eba04bf47819e9089c2c87578333eed6724c
Micaella Colombo <micaelc@g.clemson.edu>
2021-11-17 21:00:01 -0500

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  use Rack::Flash
 
   configure do
+    enable :sessions
+    set :session_secret, "travelapp-3290"
+
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -10,4 +16,20 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def authentication_required
+      if logged_in?
+        flash[:notice] = "You must be logged in."
+        redirect '/'
+      end
+    end
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,9 +9,28 @@ class UsersController < ApplicationController
         @user.password = params[:password]
 
         if @user.save
-            redirect "/"
+            redirect "/login"
         else
             erb :'users/new.html'
         end
+    end
+
+    get '/login' do
+        erb :'users/login.html'
+    end
+
+    post '/login' do #authenticate email and password. If UN and PW are valid it takes them to home page. If false, asks user to login again.
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session["user_id"] = @user.id
+            redirect "/"
+        else
+            erb :'users/login.html'
+        end 
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 end
